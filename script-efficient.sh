9e6d2893ccd5350037568fa7d8d52033da6ff7f7
Gabor Szarnyas <szarnyasg@gmail.com>
2013-09-09 14:41:51 +0200

    refactoring for the efficient case

--- script-efficient.sh
+++ script-efficient.sh
@@ -5,4 +5,4 @@ rm result
 export FAUNUS_HOME=~/faunus-0.3.2
 $FAUNUS_HOME/bin/gremlin.sh < faunus-edges-efficient.txt
 
-cat result | sort > results-efficient.txt
+cat results-efficient-unsorted.txt | sort > results-efficient.txt
